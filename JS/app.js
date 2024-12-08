d97732989c930264cb73ffa08dcd9930ff7cd2d1
costabile12 <franco_costabile@hotmail.com>
2024-12-07 21:54:10 -0300

    Cambio de header con bootstrap y agregue el carrusel

---
+++ JS/app.js
@@ -0,0 +1,7 @@
+buscar(){
+    let buscadorContainer =document.getElementById("lupa");
+    let buscador = document.createElement("input");
+    buscador.innerHTML = `
+        <input type="search">
+    `;
+}
